ee5cc615f246e8ad4a29bb99b5af1454362ff9c1
LZagatto <leozagatto@gmail.com>
2022-08-27 21:20:40 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,388 @@
+var displayOne = {
+  zero: function () {
+    this.lightsOut();
+    document.querySelector(".A1").style.display = "block";
+    document.querySelector(".B1").style.display = "block";
+    document.querySelector(".C1").style.display = "block";
+    document.querySelector(".D1").style.display = "block";
+    document.querySelector(".E1").style.display = "block";
+    document.querySelector(".F1").style.display = "block";
+   },
+  one: function () {
+    this.lightsOut();
+    document.querySelector(".B1").style.display = "block";
+    document.querySelector(".C1").style.display = "block";
+  },
+  two: function () {
+    this.lightsOut();
+    document.querySelector(".A1").style.display = "block";
+    document.querySelector(".B1").style.display = "block";
+    document.querySelector(".D1").style.display = "block";
+    document.querySelector(".E1").style.display = "block";
+    document.querySelector(".G1").style.display = "block";
+    document.querySelector(".F1").style.display = "none";
+
+  },
+  three: function () {
+    this.lightsOut();
+    document.querySelector(".A1").style.display = "block";
+    document.querySelector(".B1").style.display = "block";
+    document.querySelector(".C1").style.display = "block";
+    document.querySelector(".D1").style.display = "block";
+    document.querySelector(".G1").style.display = "block";
+  },
+  four: function () {
+    this.lightsOut();
+    document.querySelector(".B1").style.display = "block";
+    document.querySelector(".C1").style.display = "block";
+    document.querySelector(".F1").style.display = "block";
+    document.querySelector(".G1").style.display = "block";
+  },
+  five: function () {
+    this.lightsOut();
+    document.querySelector(".A1").style.display = "block";
+    document.querySelector(".C1").style.display = "block";
+    document.querySelector(".D1").style.display = "block";
+    document.querySelector(".F1").style.display = "block";
+    document.querySelector(".G1").style.display = "block";
+  },
+  six: function () {
+    this.lightsOut();
+    document.querySelector(".A1").style.display = "block";
+    document.querySelector(".C1").style.display = "block";
+    document.querySelector(".D1").style.display = "block";
+    document.querySelector(".E1").style.display = "block";
+    document.querySelector(".F1").style.display = "block";
+    document.querySelector(".G1").style.display = "block";
+  },
+  seven: function () {
+    this.lightsOut();
+    document.querySelector(".A1").style.display = "block";
+    document.querySelector(".B1").style.display = "block";
+    document.querySelector(".C1").style.display = "block";
+  },
+  eight: function () {
+    this.lightsOut();
+    document.querySelector(".A1").style.display = "block";
+    document.querySelector(".B1").style.display = "block";
+    document.querySelector(".C1").style.display = "block";
+    document.querySelector(".D1").style.display = "block";
+    document.querySelector(".E1").style.display = "block";
+    document.querySelector(".F1").style.display = "block";
+    document.querySelector(".G1").style.display = "block";
+  },
+  nine: function () {
+    this.lightsOut();
+    document.querySelector(".A1").style.display = "block";
+    document.querySelector(".B1").style.display = "block";
+    document.querySelector(".C1").style.display = "block";
+    document.querySelector(".D1").style.display = "block";
+    document.querySelector(".F1").style.display = "block";
+    document.querySelector(".G1").style.display = "block";
+
+  },
+   lightsOut: function () {
+     document.querySelector(".A1").style.display = "none";
+     document.querySelector(".B1").style.display = "none";
+     document.querySelector(".C1").style.display = "none";
+     document.querySelector(".D1").style.display = "none";
+     document.querySelector(".E1").style.display = "none";
+     document.querySelector(".G1").style.display = "none";
+     document.querySelector(".F1").style.display = "none";
+    //  document.querySelector(".E").style.opacity = 0.7;
+
+   }
+};
+
+var displayTwo = {
+  zero: function () {
+    this.lightsOut();
+    document.querySelector(".A2").style.display = "block";
+    document.querySelector(".B2").style.display = "block";
+    document.querySelector(".C2").style.display = "block";
+    document.querySelector(".D2").style.display = "block";
+    document.querySelector(".E2").style.display = "block";
+    document.querySelector(".F2").style.display = "block";
+   },
+  one: function () {
+    this.lightsOut();
+    document.querySelector(".B2").style.display = "block";
+    document.querySelector(".C2").style.display = "block";
+  },
+  two: function () {
+    this.lightsOut();
+    document.querySelector(".A2").style.display = "block";
+    document.querySelector(".B2").style.display = "block";
+    document.querySelector(".D2").style.display = "block";
+    document.querySelector(".E2").style.display = "block";
+    document.querySelector(".G2").style.display = "block";
+    document.querySelector(".F2").style.display = "none";
+
+  },
+  three: function () {
+    this.lightsOut();
+    document.querySelector(".A2").style.display = "block";
+    document.querySelector(".B2").style.display = "block";
+    document.querySelector(".C2").style.display = "block";
+    document.querySelector(".D2").style.display = "block";
+    document.querySelector(".G2").style.display = "block";
+  },
+  four: function () {
+    this.lightsOut();
+    document.querySelector(".B2").style.display = "block";
+    document.querySelector(".C2").style.display = "block";
+    document.querySelector(".F2").style.display = "block";
+    document.querySelector(".G2").style.display = "block";
+  },
+  five: function () {
+    this.lightsOut();
+    document.querySelector(".A2").style.display = "block";
+    document.querySelector(".C2").style.display = "block";
+    document.querySelector(".D2").style.display = "block";
+    document.querySelector(".F2").style.display = "block";
+    document.querySelector(".G2").style.display = "block";
+  },
+  six: function () {
+    this.lightsOut();
+    document.querySelector(".A2").style.display = "block";
+    document.querySelector(".C2").style.display = "block";
+    document.querySelector(".D2").style.display = "block";
+    document.querySelector(".E2").style.display = "block";
+    document.querySelector(".F2").style.display = "block";
+    document.querySelector(".G2").style.display = "block";
+  },
+  seven: function () {
+    this.lightsOut();
+    document.querySelector(".A2").style.display = "block";
+    document.querySelector(".B2").style.display = "block";
+    document.querySelector(".C2").style.display = "block";
+  },
+  eight: function () {
+    this.lightsOut();
+    document.querySelector(".A2").style.display = "block";
+    document.querySelector(".B2").style.display = "block";
+    document.querySelector(".C2").style.display = "block";
+    document.querySelector(".D2").style.display = "block";
+    document.querySelector(".E2").style.display = "block";
+    document.querySelector(".F2").style.display = "block";
+    document.querySelector(".G2").style.display = "block";
+  },
+  nine: function () {
+    this.lightsOut();
+    document.querySelector(".A2").style.display = "block";
+    document.querySelector(".B2").style.display = "block";
+    document.querySelector(".C2").style.display = "block";
+    document.querySelector(".D2").style.display = "block";
+    document.querySelector(".F2").style.display = "block";
+    document.querySelector(".G2").style.display = "block";
+
+  },
+   lightsOut: function () {
+     document.querySelector(".A2").style.display = "none";
+     document.querySelector(".B2").style.display = "none";
+     document.querySelector(".C2").style.display = "none";
+     document.querySelector(".D2").style.display = "none";
+     document.querySelector(".E2").style.display = "none";
+     document.querySelector(".G2").style.display = "none";
+     document.querySelector(".F2").style.display = "none";
+    //  document.querySelector(".E").style.opacity = 0.7;
+
+   }
+};
+
+var displayThree = {
+  zero: function () {
+    this.lightsOut();
+    document.querySelector(".A3").style.display = "block";
+    document.querySelector(".B3").style.display = "block";
+    document.querySelector(".C3").style.display = "block";
+    document.querySelector(".D3").style.display = "block";
+    document.querySelector(".E3").style.display = "block";
+    document.querySelector(".F3").style.display = "block";
+   },
+  one: function () {
+    this.lightsOut();
+    document.querySelector(".B3").style.display = "block";
+    document.querySelector(".C3").style.display = "block";
+  },
+  two: function () {
+    this.lightsOut();
+    document.querySelector(".A3").style.display = "block";
+    document.querySelector(".B3").style.display = "block";
+    document.querySelector(".D3").style.display = "block";
+    document.querySelector(".E3").style.display = "block";
+    document.querySelector(".G3").style.display = "block";
+    document.querySelector(".F3").style.display = "none";
+
+  },
+  three: function () {
+    this.lightsOut();
+    document.querySelector(".A3").style.display = "block";
+    document.querySelector(".B3").style.display = "block";
+    document.querySelector(".C3").style.display = "block";
+    document.querySelector(".D3").style.display = "block";
+    document.querySelector(".G3").style.display = "block";
+  },
+  four: function () {
+    this.lightsOut();
+    document.querySelector(".B3").style.display = "block";
+    document.querySelector(".C3").style.display = "block";
+    document.querySelector(".F3").style.display = "block";
+    document.querySelector(".G3").style.display = "block";
+  },
+  five: function () {
+    this.lightsOut();
+    document.querySelector(".A3").style.display = "block";
+    document.querySelector(".C3").style.display = "block";
+    document.querySelector(".D3").style.display = "block";
+    document.querySelector(".F3").style.display = "block";
+    document.querySelector(".G3").style.display = "block";
+  },
+  six: function () {
+    this.lightsOut();
+    document.querySelector(".A3").style.display = "block";
+    document.querySelector(".C3").style.display = "block";
+    document.querySelector(".D3").style.display = "block";
+    document.querySelector(".E3").style.display = "block";
+    document.querySelector(".F3").style.display = "block";
+    document.querySelector(".G3").style.display = "block";
+  },
+  seven: function () {
+    this.lightsOut();
+    document.querySelector(".A3").style.display = "block";
+    document.querySelector(".B3").style.display = "block";
+    document.querySelector(".C3").style.display = "block";
+  },
+  eight: function () {
+    this.lightsOut();
+    document.querySelector(".A3").style.display = "block";
+    document.querySelector(".B3").style.display = "block";
+    document.querySelector(".C3").style.display = "block";
+    document.querySelector(".D3").style.display = "block";
+    document.querySelector(".E3").style.display = "block";
+    document.querySelector(".F3").style.display = "block";
+    document.querySelector(".G3").style.display = "block";
+  },
+  nine: function () {
+    this.lightsOut();
+    document.querySelector(".A3").style.display = "block";
+    document.querySelector(".B3").style.display = "block";
+    document.querySelector(".C3").style.display = "block";
+    document.querySelector(".D3").style.display = "block";
+    document.querySelector(".F3").style.display = "block";
+    document.querySelector(".G3").style.display = "block";
+
+  },
+   lightsOut: function () {
+     document.querySelector(".A3").style.display = "none";
+     document.querySelector(".B3").style.display = "none";
+     document.querySelector(".C3").style.display = "none";
+     document.querySelector(".D3").style.display = "none";
+     document.querySelector(".E3").style.display = "none";
+     document.querySelector(".G3").style.display = "none";
+     document.querySelector(".F3").style.display = "none";
+    //  document.querySelector(".E").style.opacity = 0.7;
+
+   }
+};
+
+displayOne.lightsOut();
+displayTwo.lightsOut();
+displayThree.lightsOut();
+
+function findNumber() {
+  var inputValue = document.getElementById("number").value;
+  var separateNumbers = [];
+  separateNumbers = inputValue.split("");
+  
+  if(separateNumbers.length == "1"){
+    displayOne.lightsOut();
+    displayTwo.lightsOut();
+    if(separateNumbers[0] != undefined || separateNumbers[0] != ""){
+      if(separateNumbers[0] == 1) displayThree.one();
+      if(separateNumbers[0] == 2) displayThree.two();
+      if(separateNumbers[0] == 3) displayThree.three();
+      if(separateNumbers[0] == 4) displayThree.four();
+      if(separateNumbers[0] == 5) displayThree.five();
+      if(separateNumbers[0] == 6) displayThree.six();
+      if(separateNumbers[0] == 7) displayThree.seven();
+      if(separateNumbers[0] == 8) displayThree.eight();
+      if(separateNumbers[0] == 9) displayThree.nine();
+      if(separateNumbers[0] == 0) displayThree.zero();
+    }
+  }
+
+if(separateNumbers.length == "2"){
+  displayOne.lightsOut();
+  if(separateNumbers[0] != undefined || separateNumbers[0] != ""){
+    if(separateNumbers[0] == 1) displayTwo.one();
+    if(separateNumbers[0] == 2) displayTwo.two();
+    if(separateNumbers[0] == 3) displayTwo.three();
+    if(separateNumbers[0] == 4) displayTwo.four();
+    if(separateNumbers[0] == 5) displayTwo.five();
+    if(separateNumbers[0] == 6) displayTwo.six();
+    if(separateNumbers[0] == 7) displayTwo.seven();
+    if(separateNumbers[0] == 8) displayTwo.eight();
+    if(separateNumbers[0] == 9) displayTwo.nine();
+    if(separateNumbers[0] == 0) displayTwo.zero();
+  }
+  
+  if(separateNumbers[1] != undefined || separateNumbers[1] != ""){
+    if(separateNumbers[1] == 1) displayThree.one();
+    if(separateNumbers[1] == 2) displayThree.two();
+    if(separateNumbers[1] == 3) displayThree.three();
+    if(separateNumbers[1] == 4) displayThree.four();
+    if(separateNumbers[1] == 5) displayThree.five();
+    if(separateNumbers[1] == 6) displayThree.six();
+    if(separateNumbers[1] == 7) displayThree.seven();
+    if(separateNumbers[1] == 8) displayThree.eight();
+    if(separateNumbers[1] == 9) displayThree.nine();
+    if(separateNumbers[1] == 0) displayThree.zero();
+  }
+}
+
+if(separateNumbers.length == "3"){
+  if(separateNumbers[0] != undefined || separateNumbers[2] != ""){
+    if(separateNumbers[0] == 1) displayOne.one();
+    if(separateNumbers[0] == 2) displayOne.two();
+    if(separateNumbers[0] == 3) displayOne.three();
+    if(separateNumbers[0] == 4) displayOne.four();
+    if(separateNumbers[0] == 5) displayOne.five();
+    if(separateNumbers[0] == 6) displayOne.six();
+    if(separateNumbers[0] == 7) displayOne.seven();
+    if(separateNumbers[0] == 8) displayOne.eight();
+    if(separateNumbers[0] == 9) displayOne.nine();
+    if(separateNumbers[0] == 0) displayOne.zero();
+  }
+  if(separateNumbers[1] != undefined || separateNumbers[0] != ""){
+    if(separateNumbers[1] == 1) displayTwo.one();
+    if(separateNumbers[1] == 2) displayTwo.two();
+    if(separateNumbers[1] == 3) displayTwo.three();
+    if(separateNumbers[1] == 4) displayTwo.four();
+    if(separateNumbers[1] == 5) displayTwo.five();
+    if(separateNumbers[1] == 6) displayTwo.six();
+    if(separateNumbers[1] == 7) displayTwo.seven();
+    if(separateNumbers[1] == 8) displayTwo.eight();
+    if(separateNumbers[1] == 9) displayTwo.nine();
+    if(separateNumbers[1] == 0) displayTwo.zero();
+  }
+  
+  if(separateNumbers[2] != undefined || separateNumbers[1] != ""){
+    if(separateNumbers[2] == 1) displayThree.one();
+    if(separateNumbers[2] == 2) displayThree.two();
+    if(separateNumbers[2] == 3) displayThree.three();
+    if(separateNumbers[2] == 4) displayThree.four();
+    if(separateNumbers[2] == 5) displayThree.five();
+    if(separateNumbers[2] == 6) displayThree.six();
+    if(separateNumbers[2] == 7) displayThree.seven();
+    if(separateNumbers[2] == 8) displayThree.eight();
+    if(separateNumbers[2] == 9) displayThree.nine();
+    if(separateNumbers[2] == 0) displayThree.zero();
+  }
+}
+  if(separateNumbers.length > 3){
+console.log("ai nao dá");
+}
+
+}
+// document.getElementById("demo").innerHTML = toCelsius; 
